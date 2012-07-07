c3978efa6df63d0e20773cab23e1190a0e2c2d20
clux <analsandblaster@gmail.com>
2012-07-07 19:01:05 +0100

    src tweaks

--- typr.js
+++ typr.js
@@ -1,53 +1,55 @@
-var toStr = Object.prototype.toString;
+var t = {}
+  , toStr = Object.prototype.toString;
 
-// All JS types that can be simply tested with toStr on node 0.4 and node >=0.6
-var types = [
+// All JS types that can be simply tested with toStr on node >= 0.4
+[
   'Function'
-, 'Object'    // Object(obj) === obj used in underscore is also true for other things like noop
+, 'Object'    // Object(obj) === obj is not simply true for basetype Object elements
 , 'Date'
 , 'Number'    // Note: Number includes {NaN, Infinity, -Infinity} (but these can be tested for specifically below)
-, 'String'    // string can  be literal and Object
+, 'String'
 , 'Boolean'
 , 'RegExp'
 , 'Arguments' // should probably not use this - arguments going away
-];
-
-types.forEach(function (type) {
+]
+.forEach(function (type) {
   var expected = '[object ' + type + ']';
-  exports['is' + type] = function (o) {
+  t['is' + type] = function (o) {
     return toStr.call(o) === expected;
   };
 });
 
 // Do this faster
-exports.isArray = function (o) {
+t.isArray = function (o) {
   return Array.isArray(o);
 };
 
 // toStr.call fails for null on node 0.4
-exports.isNull = function (o) {
+t.isNull = function (o) {
   return o === null;
 };
 
 // toStr.call fails for undefined on node 0.4
-exports.isUndefined = function (o) {
+t.isUndefined = function (o) {
   var undef;
   return o === undef;
 };
 
 // This is an extra test, as NaN is (technically) a Number
 // However, NaN is the only value for which === is not reflexive
-exports.isNaN = function (n) {
+t.isNaN = function (n) {
   return n !== n;
 };
 
 // +- Infinity is also a Number so this is also an extra test
-exports.isInfinite = function (n) {
+t.isInfinite = function (n) {
   return Math.abs(n) === Infinity;
 };
 
 // does not follow the partition, but sometimes this is rather what we want
 // functions can have properties => they are Object like, with keys() defined
-exports.hasKeys = function (o) {
+t.hasKeys = function (o) {
   return Object(o) === o;
 };
+
+module.exports = t;
